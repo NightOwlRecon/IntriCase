2b97354e4f17b4120a7519c5fa2ec91066f3f9f7
Nick Price <nick@spun.io>
2024-05-25 12:53:46 -0700

    think this makes more sense

--- src/api/users.rs
+++ src/api/users.rs
@@ -6,12 +6,12 @@ use axum::routing::get;
 use axum::Router;
 
 pub fn router() -> Router<AppState> {
-    Router::new().route("/", get(get_all))
+    Router::new().route("/", get(get_enabled))
 }
 
 // TODO: this is duplicated in the `admin` module - check fields that need to be masked for non-admins
-pub async fn get_all(State(state): State<AppState>) -> impl IntoResponse {
-    if let Ok(users) = users::get_all_enabled(&state.db).await {
+pub async fn get_enabled(State(state): State<AppState>) -> impl IntoResponse {
+    if let Ok(users) = users::get_enabled(&state.db).await {
         return axum::Json(users).into_response();
     }
     axum::http::StatusCode::INTERNAL_SERVER_ERROR.into_response()
