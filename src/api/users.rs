e2622f8dd09052eddabcdfbfde7bd1300130991e
Nick Price <nick@spun.io>
2024-05-25 12:52:57 -0700

    only enabled users lol

--- src/api/users.rs
+++ src/api/users.rs
@@ -11,7 +11,7 @@ pub fn router() -> Router<AppState> {
 
 // TODO: this is duplicated in the `admin` module - check fields that need to be masked for non-admins
 pub async fn get_all(State(state): State<AppState>) -> impl IntoResponse {
-    if let Ok(users) = users::get_all(&state.db).await {
+    if let Ok(users) = users::get_all_enabled(&state.db).await {
         return axum::Json(users).into_response();
     }
     axum::http::StatusCode::INTERNAL_SERVER_ERROR.into_response()
